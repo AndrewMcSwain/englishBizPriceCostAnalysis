74002631f61e12a6a3f08e5e446d6b6038609373
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-05 15:26:20 -0200

    Update margin.js

--- margin.js
+++ margin.js
@@ -1,5 +1,18 @@
 function (margin){
-var priceArray=[];
+var price = {
+  startingValue : 200;
+  increment : function (){prompt("what is your monthly price increment?", "ex: 10");
+  calculate: 
+    function(){ 
+      for(i = 0; i < classSize.max; i++){
+         var monthlyPayment = 
+              if(priceArray == true){ //does an empty array return true?
+                  priceArrayStartingValue * 1}
+              else {priceArrayStartingValue * priceArrayIncrement};
+           priceArray.push(monthlyPayment);
+      },
+        
+  array: [];
 var priceArrayStartingValue = 200;
 var classSize = { min: 3, max: 8 };
 var studentQtyArray= [2,3,4,5,6,7,8];
@@ -7,15 +20,15 @@ var studentQtyArrayLength = studentQtyArray.length;
 var resultArray=[];
 var priceArrayIncrement=function (){prompt("what is your monthly price increment?", "ex: 10");
 
-function calculate(){ 
+function calculatePriceArray(){ 
   for(i = 0; i < classSize.max; i++){
-       var monthlyPayment = if(priceArray == true){ //does an empty array return true?
-      	priceArrayStartingValue * 
-          
-          
-         priceArrayStartingValue * priceArrayIncrement;
-    priceArray.push(monthlyPayment);
-for(i = 0; i < studentQtyArrayLength; i++) 
-priceArray[i] * priceArray
+       var monthlyPayment = 
+       		if(priceArray == true){ //does an empty array return true?
+      			priceArrayStartingValue * 1}
+       		else {priceArrayStartingValue * priceArrayIncrement};
+   		 priceArray.push(monthlyPayment);
+
+  for(i = 0; i < studentQtyArrayLength; i++) 
+	priceArray[i] *
  
 }
