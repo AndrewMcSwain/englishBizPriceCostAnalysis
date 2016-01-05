d8defbda16e65f52787968afe23afab86914ea9a
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-05 17:48:41 -0200

    Update margin.js

--- margin.js
+++ margin.js
@@ -1,33 +1,65 @@
 function (margin){
 
+
+	//prompt
+	function (){prompt("what is your monthly price increment?", "ex: 10");}
+	
+	var promptFn= function(prop, varb){  
+	 if (varb != undefined){
+	 prompt("What is the " + var + " of " + prop?");                                    
+	      var propString = prop.toString();
+	      var varbString = varb.toString();
+	     var newName = propString + varbString;  
+	     prompt("What is the " + var + " of " + prop + "?"); //how to set the variable name to the string value stored in var newName?
+	 }
+	 else prompt("What is the " + prop?");
+	}
+	
+	function
+
 var price = {
-  startingValue : 200;
+  startingvalue : 200;
   array: [],
-  priceincrement : function (){prompt("what is your monthly price increment?", "ex: 10");}
-  calculate: = 
-    function(){ 
-      for(i = 0; i < classSize.max; i++){
-         var monthlyPayment = [];
+  valueincrement : 
+  calculate: = {
+    function (){ 
+ 	//global variables inside of calculate 
+      var monthlyPayment = [];
+      function unitary(){
+        for(i = 0; i < classSize.max; i++){
               if(price.array == true){ //does an empty array return true?
-                  price.startingValue * 1}
-              else {priceArrayStartingValue * priceArrayIncrement};
+                  price.startingValue * 1};
+              else {price.startingvalue * price.valueincrement};
            priceArray.push(monthlyPayment);
               }
     	return monthlyPayment;
-           },
-       };
-
+           }
+      ,//comma of object syntax 
+        
+     //new item in object calculate
+     function aggregate(monthly, classsize, classqty){
+       sales = {
+         grosssales : monthlyAmount * classsize * classqty,
+         netsales : grosssales - expense.teacher,
+         }; //closes aggregate.sales
+ 	 }//closes function price.calculate.aggregate()
+    }//closes calculate.function() (the removal of which is being considered)
+  }//closes price.calculate
+}//closes object price
+                       
+var expense = {
+  teacher : 680,
+               };
+                       
 var classSize = { min: 3, max: 8 };
-
+                       
 var studentQtyArray= [2,3,4,5,6,7,8];
-
 var studentQtyArrayLength = studentQtyArray.length;
-
 var priceList = price.calculate();
-
 var resultArray = studentQtyCalc();
     
 for(i = 0; i < studentQtyArrayLength; i++) 
 	
  
 }
+
