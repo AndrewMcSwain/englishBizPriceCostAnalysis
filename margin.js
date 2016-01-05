3528c68edf5ecdf3a36b12c1057ef2a5106621cc
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-05 14:25:21 -0200

    Create margin.js

---
+++ margin.js
@@ -0,0 +1,18 @@
+function (margin){
+var priceArray=[];
+var priceArrayStartingValue = 200;
+var classSize = {
+  min: 
+var max: ;
+var studentQtyArray= [2,3,4,5,6,7,8];
+var studentQtyArrayLength = studentQtyArray.length;
+var resultArray=[];
+var priceArrayIncrement=function (){prompt("what is your monthly price increment?", "ex: 10");
+
+var prompt = function(prop, varb){  
+  
+  prompt("What is the " + var + " of " + prop?");                                    
+for(i = 0; i < studentQtyArrayLength; i++) 
+priceArray[i] * 
+ 
+}
