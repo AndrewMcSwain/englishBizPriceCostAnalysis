438a8eec45ff429d8dfeab177091231cbb07b4c2
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-05 15:37:18 -0200

    Update margin.js

--- margin.js
+++ margin.js
@@ -1,34 +1,33 @@
 function (margin){
+
 var price = {
   startingValue : 200;
-  increment : function (){prompt("what is your monthly price increment?", "ex: 10");
-  calculate: 
+  array: [],
+  priceincrement : function (){prompt("what is your monthly price increment?", "ex: 10");}
+  calculate: = 
     function(){ 
       for(i = 0; i < classSize.max; i++){
-         var monthlyPayment = 
-              if(priceArray == true){ //does an empty array return true?
-                  priceArrayStartingValue * 1}
+         var monthlyPayment = [];
+              if(price.array == true){ //does an empty array return true?
+                  price.startingValue * 1}
               else {priceArrayStartingValue * priceArrayIncrement};
            priceArray.push(monthlyPayment);
-      },
-        
-  array: [];
-var priceArrayStartingValue = 200;
+              }
+    	return monthlyPayment;
+           },
+       };
+
 var classSize = { min: 3, max: 8 };
+
 var studentQtyArray= [2,3,4,5,6,7,8];
+
 var studentQtyArrayLength = studentQtyArray.length;
-var resultArray=[];
-var priceArrayIncrement=function (){prompt("what is your monthly price increment?", "ex: 10");
 
-function calculatePriceArray(){ 
-  for(i = 0; i < classSize.max; i++){
-       var monthlyPayment = 
-       		if(priceArray == true){ //does an empty array return true?
-      			priceArrayStartingValue * 1}
-       		else {priceArrayStartingValue * priceArrayIncrement};
-   		 priceArray.push(monthlyPayment);
+var priceList = price.calculate();
 
-  for(i = 0; i < studentQtyArrayLength; i++) 
-	priceArray[i] *
+var resultArray = studentQtyCalc();
+    
+for(i = 0; i < studentQtyArrayLength; i++) 
+	
  
 }
