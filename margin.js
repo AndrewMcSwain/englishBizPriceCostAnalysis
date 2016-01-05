716b89d2e43d33a7b1af2ef3201856e8e5cbfcf8
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-05 15:15:47 -0200

    Update margin.js

--- margin.js
+++ margin.js
@@ -1,25 +1,21 @@
 function (margin){
 var priceArray=[];
 var priceArrayStartingValue = 200;
-var classSize = {
-  min: 
-var max: ;
+var classSize = { min: 3, max: 8 };
 var studentQtyArray= [2,3,4,5,6,7,8];
 var studentQtyArrayLength = studentQtyArray.length;
 var resultArray=[];
 var priceArrayIncrement=function (){prompt("what is your monthly price increment?", "ex: 10");
 
-var promptFn= function(prop, varb){  
-  if (varb != undefined){
-       var propString = prop.toString();
-       var varbString = varb.toString();
-       var newName = propString + varbString;  
-      prompt("What is the " + var + " of " + prop + "?"); //how to set the variable name to the string value stored in var newName?
-  }
-  else prompt("What is the " + prop?");
-}
-
+function calculate(){ 
+  for(i = 0; i < classSize.max; i++){
+       var monthlyPayment = if(priceArray == true){ //does an empty array return true?
+      	priceArrayStartingValue * 
+          
+          
+         priceArrayStartingValue * priceArrayIncrement;
+    priceArray.push(monthlyPayment);
 for(i = 0; i < studentQtyArrayLength; i++) 
-priceArray[i] * 
+priceArray[i] * priceArray
  
 }
