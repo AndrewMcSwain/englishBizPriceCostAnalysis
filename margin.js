700c5706ef1e661e8bcf608215e4493d971b29f0
AndrewMcSwain <andrew@e4b.us>
2016-01-11 18:43:18 +0000

    code organization and unitaryClass
    changed some variable names, added second coordinate to MD array
    reorganized code

--- margin.js
+++ margin.js
@@ -1,13 +1,63 @@
 function (margin){
- 
+ //global variables
   var classSize = { min: 3, max: 8 };
-			 
+  var expense = {teacher : 680};
   var studentQtyArray= [2,3,4,5,6,7,8];
-  var studentQtyArrayLength = studentQtyArray.length;
-  //var priceList = price.calculate();
-  //var resultArray = studentQtyCalc();
-   
+  var studentQtyArrayLength = studentQtyArray.length; 
+  var price = {
+    startingvalue: 200,
+    array: [],
+    valueincrement: 20, 
+    max: 500,
+    calculate: {
+        monthlypaymentstudent : [],// (200, 220, 240, 260...)
+        monthlypaymentclass : [],
 
+        unitaryStudent: function unitaryStudentFn(){
+          var student = price.calculate.monthlypaymentstudent;
+          for(i = price.startingvalue; i <= price.max; i += price.valueincrement) student.push(i);
+          return student;
+        },
+
+         unitaryClass: function unitaryClassFn(){
+          var counter = 0;
+          var student = price.calculate.monthlypaymentstudent; //is an array [200,220,240]
+          var classArray = price.calculate.monthlypaymentclass;//an empty array, to be filled by this funtion
+          var possiblePricesArray = price.calculate.monthlypaymentstudent.length; //length of the array
+          //var lengthClass = price.calculate.monthlypaymentclass.length;
+          var result = 0;
+    
+          while(counter < possiblePricesArray){
+            for(i = classSize.min; i <= classSize.max; i++){
+	       var secondCounter = 0;
+               result = i * student[counter];
+               classArray[counter][secondCounter].push(result);  
+	             secondCounter++;
+            }
+            counter++;
+          }
+          return classArray;
+        },  
+     //new item in object calculate
+     analysis: function (monthly, classsize, classqty){
+       sales = {
+         grosssales : monthlyAmount * classsize * classqty,
+         netsales : grosssales - expense.teacher,
+         } //price.calculate.analysis.sales
+ 	 }//price.calculate.analysis()
+    } 
+  }    
+        
+
+  }//price.calculate
+}   
+	
+
+
+
+//non-functioning modules 
+
+/*
 	//prompt
 	function(){prompt("what is your monthly price increment?", "ex: 10");}
 	
@@ -23,66 +73,5 @@ function (margin){
 		 ;
 	return promptReturn;
 	}	
-
-  var price = {
-    startingvalue: 200,
-    array: [],
-    valueincrement: 20, 
-    max: 500,
-    calculate: {
-        monthlypaymentstudent : [],// (200, 220, 240, 260...)
-        monthlypaymentclass : [],
-
-        unitaryStudent: function unitaryStudentFn(){
-          var student = price.calculate.monthlypaymentstudent;
-          for(i = price.startingvalue; i <= price.max; i += price.valueincrement)student.push(i);
-          return student;
-        },
-
-         unitaryClass: function unitaryClassFn(){
-          var counter = 0;
-          var student = price.calculate.monthlypaymentstudent; //is an array [200,220,240]
-          var aula = price.calculate.monthlypaymentclass;//an empty array, to be filled by this funtion
-          var lengthStudent = price.calculate.monthlypaymentstudent.length; //length of the array
-          //var lengthClass = price.calculate.monthlypaymentclass.length;
-          var result = 0;
-    
-          while(counter < lengthStudent){
-            console.log("before: "+ counter)
-            for(i = classSize.min; i <= classSize.max; i++){
-              result = i * student[counter]
-              aula[counter].push(result);  
-            }
-            counter++;
-            console.log("after " + counter);
-          
-          }
-          return aula;
-        }
-    }
-  }
-
-console.log(price.calculate.unitaryStudent());
-      ,  //comma of object syntax    
-    
-      //new item in object calculate
-     function analysis(monthly, classsize, classqty){
-       sales = {
-         grosssales : monthlyAmount * classsize * classqty,
-         netsales : grosssales - expense.teacher,
-         } //closes aggregate.sales
- 	 }//closes function price.calculate.aggregate()
-    }//closes calculate.function() (the removal of which is being considered)
-  }//closes price.calculate
-}//closes object price
-                       
-var expense = {
-  teacher : 680,
-               };
-                       
-}   
-/* I want to determine the 
-	
+*/
  
-}
-
